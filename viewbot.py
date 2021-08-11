4caf60ad678e84df89e63b54e59dc43c1408e5e3
Linus <83720412+LinusUsernameTips@users.noreply.github.com>
2021-08-11 09:40:22 -0400

    Create viewbot.py

---
+++ viewbot.py
@@ -0,0 +1,23 @@
+import requests
+import threading
+headers = {'Cookie': 'D',
+            'User-Agent': '',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'}
+user = input('Who to bot? : ')
+def Analytics_Bot():
+    while True:
+        r = requests.get(f'https://solo.to/{user}', headers=headers)
+        print(r.status_code)
+try:
+      threads = []
+      for i in range(20):
+                t = threading.Thread(target=Analytics_Bot)
+                t.daemon = True
+                t.start()
+                threads.append(t)
+      for thread in threads:
+                thread.join()
+except:
+  print('Error starting threads or some shit idfk')
